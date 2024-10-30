92e8f2f140dc0bdd44ba4e153076be5b111634c5
igor <mino@tpsoft.org>
2024-10-30 14:18:37 +0100

    added first version of plugin

---
+++ background.js
@@ -0,0 +1,67 @@
+// Thunderbird can terminate idle backgrounds in Manifest V3.
+// Any listener directly added during add-on startup will be registered as a
+// persistent listener and the background will wake up (restart) each time the
+// event is fired.
+
+browser.messageDisplay.onMessageDisplayed.addListener((tab, message) => {
+	// console.log(`Message displayed in tab ${tab.id}: ${message.subject}`);
+});
+
+function appendFormData(formData, data, parentKey = '') {
+	if (data && typeof data === 'object' && !(data instanceof Date) && !(data instanceof File)) {
+		Object.keys(data).forEach(key => {
+			appendFormData(formData, data[key], parentKey ? `${parentKey}[${key}]` : key);
+		});
+	} else {
+		formData.append(parentKey, data);
+	}
+}
+
+function webservice(service, params, callback) {
+	var webservice_endpoint_url = localStorage.getItem('webservice_endpoint_url')+'?ws='+service;
+	var form_data  = new FormData();
+	// Object.keys(params).forEach(key => {
+	// 	form_data.append(key, params[key]);
+	// });
+	appendFormData(form_data, params);
+	fetch(webservice_endpoint_url, {
+		method: 'POST',
+		body: form_data
+	})
+	.then(response => response.json())
+	.then(json => {
+		if (callback != null) callback(json);
+	})
+	.catch(error => console.error('Error loading MMDB:', error));
+}
+
+function antispamAdd(maildata, callback) {
+	webservice('antispam', {action: 'add', maildata: maildata}, function(response){
+		var result = {};
+		if (response.status == 'OK') {
+			result = {'success': true, 'result': response.data};
+		} else {
+			console.error(response.msg);
+			result = {'success': false, 'message': response.msg};
+		}
+		if (callback != null) callback(result);
+	});
+}
+
+
+browser.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		//console.log(request);
+		switch(request.name) {
+			case 'antispamAdd':
+				antispamAdd(request.maildata, function(result){
+					sendResponse(result);
+				});
+				break;
+			default:
+				sendResponse({});
+				break;
+		}
+		return true;
+	}
+);
---
+++ popup.js
@@ -0,0 +1,51 @@
+browser.tabs.query({
+	active: true,
+	currentWindow: true,
+}).then(tabs => {
+	let tabId = tabs[0].id;
+	browser.messageDisplay.getDisplayedMessages(tabId).then(([message]) => {
+		// console.log(message);
+		document.getElementById('antispam_recipients').value = message.recipients.join(', ');
+		document.getElementById('antispam_sender').value = message.author;
+		document.getElementById('antispam_subject').value = message.subject;
+	});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+	/* ----------------------------------------------------
+	 * Initialize
+	 */
+
+	/* ----------------------------------------------------
+	 * Button Send onClick
+	 */
+	document.getElementById('antispam_button_send').addEventListener('click', function() {
+		var maildata = {
+			recipients: document.getElementById('antispam_recipients').value,
+			sender: document.getElementById('antispam_sender').value,
+			subject: document.getElementById('antispam_subject').value
+		};
+		document.body.textContent = 'Sending...';
+		antispamAdd(maildata);
+	});
+
+});
+
+
+function antispamAdd(maildata, callback) {
+	browser.runtime.sendMessage({'name': 'antispamAdd', 'maildata': maildata}, function(response){
+		// console.log(response);
+		var result_html = 'N/A';
+		if (response.success == true) {
+			result_html = 'OK';
+			if (response.result != null
+				&& response.result.message != null)
+			{
+				result_html += ': '.response.result.message;
+			}
+		} else {
+			result_html = response.message;
+		}
+		document.body.textContent = result_html;
+	});
+}
