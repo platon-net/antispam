92fbdf989670f6028e956b3e3e96753293f7a122
igorplaton <mino@platon.org>
2024-12-05 11:46:12 +0100

    added sender domain, reply-to with domain and ip addresses from header received, upgrade to 1.2

--- popup.js
+++ popup.js
@@ -6,9 +6,20 @@ browser.tabs.query({
 	// console.log(messenger);
 	messenger.messageDisplay.getDisplayedMessages(tabId).then((message_list) => {
 		var message = message_list.messages[0];
+		// console.log(message);
 		document.getElementById('antispam_recipients').value = message.recipients.join(', ');
 		document.getElementById('antispam_sender').value = message.author;
+		document.getElementById('antispam_sender_domain').value = getDomainFromEmail(message.author);
 		document.getElementById('antispam_subject').value = message.subject;
+		messenger.messages.getFull(message.id).then((message_part) => {
+			// console.log(message_part);
+			let replyto = message_part.headers['reply-to'];
+			if (replyto != null) {
+				document.getElementById('antispam_replyto').value = replyto.join(', ');
+				document.getElementById('antispam_replyto_domain').value = getDomainFromEmail(replyto);
+			}
+			document.getElementById('antispam_ipaddresses').value = extractIPAddresses(message_part.headers.received);
+		});
 	});
 });
 
@@ -49,4 +60,43 @@ function antispamAdd(maildata, callback) {
 		}
 		document.body.textContent = result_html;
 	});
+}
+
+function getDomainFromEmail(email) {
+	if (Array.isArray(email)) {
+		let domains = [];
+		for (let i = 0; i < email.length; i++) {
+			domains.push(getDomainFromEmail(email[i]));
+		}
+		return domains.join(', ');
+	}
+	if (email == null) return null;
+	const domain = email.split('@')[1].replace(/[<>]/g, '');
+	return domain;
+}
+
+function extractIPAddresses(text) {
+	let ignored_ip = ['127.0.0.1', '192.168.'];
+	if (Array.isArray(text)) {
+		let ips = [];
+		for (let i = 0; i < text.length; i++) {
+			ips = ips.concat(extractIPAddresses(text[i]));
+		}
+		ips = ips.reduce((acc, item) => {
+			for (let j = 0; j < ignored_ip.length; j++) {
+				if (item.startsWith(ignored_ip[j])) {
+					return acc;
+				}
+			}
+			if (!acc.includes(item)) {
+				acc.push(item);
+			}
+			return acc;
+		}, []);
+		return ips.join(', ');
+	}
+	if (text == null) return [];
+	const ipPattern = /\b(?:\d{1,3}\.){3}\d{1,3}\b/g;
+	const matches = text.match(ipPattern);
+	return matches || [];
 }
