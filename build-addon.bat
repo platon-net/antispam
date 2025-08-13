3b1ceff986b7946d36f71de2dad4735db744b995
igorplaton <mino@platon.org>
2025-08-13 11:52:32 +0200

    release v1.10

--- build-addon.bat
+++ build-addon.bat
@@ -2,7 +2,7 @@
 rem cd .\src
 
 for /f "delims=" %%a in ('findstr "\"version\"" manifest.json') do @set versionrow=%%a
-set version=%versionrow:~13,3%
+set version=%versionrow:~13,4%
 tar -a -c -f build\thunderbird\Antispam.%version%.zip --exclude "*.bat" --exclude "*.psd" --exclude "*.eps"  --exclude ".git" --exclude "build" --exclude "test" *
 
 rem cd ..
