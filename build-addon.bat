1fccf33f1dba00c1fadac618310389faa98c57d2
igorplaton <mino@platon.org>
2025-09-16 08:54:32 +0200

    renamed ZIP to XPI, added rename to XPI for build script

--- build-addon.bat
+++ build-addon.bat
@@ -4,6 +4,7 @@ rem cd .\src
 for /f "delims=" %%a in ('findstr "\"version\"" manifest.json') do @set versionrow=%%a
 set version=%versionrow:~13,4%
 tar -a -c -f build\thunderbird\Antispam.%version%.zip --exclude "*.bat" --exclude "*.psd" --exclude "*.eps"  --exclude ".git" --exclude "build" --exclude "test" *
+move build\thunderbird\Antispam.%version%.zip build\thunderbird\Antispam.%version%.xpi
 
 rem cd ..
 pause
